0c8d843c4a1e1b7e5581e90730f858efb4b8c8c9
EvgenSpinin <bodya1212121212@gmail.com>
2024-03-05 19:55:11 +0200

    add trek

--- trak-html.py
+++ trak-html.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, Response
 import cv2
 import argparse
+import curses
 
 app = Flask(__name__)
 
@@ -10,8 +11,36 @@ camera = cv2.VideoCapture(0)  # RTSP-поток
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Ширина кадра
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Высота кадра
 
+# Получение первого кадра
+ok, frame = cap.read()
+if not ok:
+    print('Cannot read video file')
+    exit()
 
+# Выбор области для трекинга
+#bbox = cv2.selectROI(frame, False)
 
+# Инициализация CSRT трекера
+#tracker = cv2.TrackerCSRT_create()
+#tracker.init(frame, bbox)
+bbox = None
+# Инициализация curses
+stdscr = curses.initscr()
+curses.noecho()
+stdscr.keypad(True)
+# Устанавливаем таймаут ввода в 0, чтобы программа не ожидала ввода с клавиатуры
+stdscr.timeout(0)
+
+# Центральные координаты и размер области интереса
+center_x = 320 // 2
+center_y = 240 // 2
+roi_size = 30
+
+# Вычисление координат углов ROI
+x1 = int(center_x - roi_size / 2)
+y1 = int(center_y - roi_size / 2)
+x2 = int(center_x + roi_size / 2)
+y2 = int(center_y + roi_size / 2)
 
 
 
@@ -24,10 +53,49 @@ def getFramesGenerator():
         success, frame = camera.read()  # Получаем кадр с камеры
         if not success:
             continue  # Пропустить кадр, если не удалось получить
+
+            # check to see if we are currently tracking an object
+        if bbox is not None:
+            # grab the new bounding box coordinates of the object
+            (success, box) = tracker.update(frame)
+            # check to see if the tracking was a success
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+
+
+
+
+
+
         frame = cv2.rotate(frame, cv2.ROTATE_180)
         _, buffer = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+
+        stdscr.refresh()
+        c = stdscr.getch()
+
+        if c == ord(" "):
+            # select the bounding box of the object we want to track (make
+            # sure you press ENTER or SPACE after selecting the ROI)
+            #initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+            #    showCrosshair=True)
+            bbox = (x1, y1, roi_size, roi_size)
+            tracker = cv2.TrackerCSRT_create()
+            tracker.init(frame, bbox)
+
+
+        # if the `q` key was pressed, break from the loop
+        elif c == ord("q"):
+            break
+
+
+
+
 
 @app.route('/video_feed')
 def video_feed():
