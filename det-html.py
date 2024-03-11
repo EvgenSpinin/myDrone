d04f35d9756caa24da42da7bb3e4c9e5b4b9cd63
EvgenSpinin <bodya1212121212@gmail.com>
2024-03-11 14:14:06 +0200

    addDETEECT

--- det-html.py
+++ det-html.py
@@ -10,6 +10,41 @@ camera = cv2.VideoCapture(0)  # RTSP-поток
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Ширина кадра
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Высота кадра
 
+#for DEtection
+classNames = []
+classFile = "/home/pi/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/pi/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+    #print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=1)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,0.35,(0,255,0),1)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+70,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,0.35,(0,255,0),1)
+
+    return img,objectInfo
+
+
 def getFramesGenerator():
     """Генератор кадров для вывода на веб-страницу."""
     while True:
@@ -17,6 +52,11 @@ def getFramesGenerator():
         if not success:
             continue  # Пропустить кадр, если не удалось получить
         frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+
+        #success, img = cap.read()
+        result, objectInfo = getObjects(frame,0.45,0.2)
+        
         _, buffer = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
